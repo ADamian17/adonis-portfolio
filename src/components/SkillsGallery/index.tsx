318dd02850af6328d30efa532a1398f51cf3b9d0
ADamian17 <adonisdesuvikdo@gmail.com>
2023-01-29 16:01:43 -0800

    gallery layout done

--- src/components/SkillsGallery/index.tsx
+++ src/components/SkillsGallery/index.tsx
@@ -12,43 +12,44 @@ const SkillsGallery: React.FC<SkillsGalleryType> = (props) => {
       </figure>
 
       <figure className={styles.skillsGalleryItem}>
-        <img src="/icons/skills/reactjs.svg" alt="gallery img 2" className={styles.skillsGalleryItemImg} />
+        <img src="/icons/skills/nodejs.svg" alt="gallery img 2" className={styles.skillsGalleryItemImg} />
       </figure>
 
       <figure className={styles.skillsGalleryItem}>
-        <img src="/icons/skills/nodejs.svg" alt="gallery img 3" className={styles.skillsGalleryItemImg} />
+        <img src="/icons/skills/javascript.svg" alt="gallery img 3" className={styles.skillsGalleryItemImg} />
       </figure>
 
       <figure className={styles.skillsGalleryItem}>
-        <img src="/icons/skills/postgresql.svg" alt="gallery img 4" className={styles.skillsGalleryItemImg} />
+        <img src="/icons/skills/expressjs-ar21.svg" alt="gallery img 4" className={styles.skillsGalleryItemImg} />
       </figure>
 
       <figure className={styles.skillsGalleryItem}>
-        <img src="/icons/skills/javascript.svg" alt="gallery img 5" className={styles.skillsGalleryItemImg} />
+        <img src="/icons/skills/python.svg" alt="gallery img 5" className={styles.skillsGalleryItemImg} />
       </figure>
 
       <figure className={styles.skillsGalleryItem}>
-        <img src="/icons/skills/sass-lang-ar21.svg" alt="gallery img 6" className={styles.skillsGalleryItemImg} />
+        <img src="/icons/skills/django.svg" alt="gallery img 6" className={styles.skillsGalleryItemImg} />
       </figure>
 
       <figure className={styles.skillsGalleryItem}>
-        <img src="/icons/skills/python-ar21.svg" alt="gallery img 7" className={styles.skillsGalleryItemImg} />
+        <img src="/icons/skills/graphql.svg" alt="gallery img 7" className={styles.skillsGalleryItemImg} />
       </figure>
 
       <figure className={styles.skillsGalleryItem}>
-        <img src="/icons/skills/djangoproject-ar21.svg" alt="gallery img 8" className={styles.skillsGalleryItemImg} />
+        <img src="/icons/skills/gatsby-logo.svg" alt="gallery img 8" className={styles.skillsGalleryItemImg} />
       </figure>
 
       <figure className={styles.skillsGalleryItem}>
-        <img src="/icons/skills/redux.svg" alt="gallery img 9" className={styles.skillsGalleryItemImg} />
+        <img src="/icons/skills/typescript.svg" alt="gallery img 9" className={styles.skillsGalleryItemImg} />
+      </figure>
+
+
+      <figure className={styles.skillsGalleryItem}>
+        <img src="/icons/skills/reactjs.svg" alt="gallery img 10" className={styles.skillsGalleryItemImg} />
       </figure>
 
       <figure className={styles.skillsGalleryItem}>
-        <img src="/icons/skills/rest-api.svg" alt="gallery img 10" className={styles.skillsGalleryItemImg} />
-      </figure>
-
-      <figure className={styles.skillsGalleryItem}>
-        <img src="/icons/skills/expressjs-ar21.svg" alt="gallery img 11" className={styles.skillsGalleryItemImg} />
+        <img src="/icons/skills/redux.svg" alt="gallery img 11" className={styles.skillsGalleryItemImg} />
       </figure>
 
       <figure className={styles.skillsGalleryItem}>
@@ -56,11 +57,44 @@ const SkillsGallery: React.FC<SkillsGalleryType> = (props) => {
       </figure>
 
       <figure className={styles.skillsGalleryItem}>
-        <img src="/icons/skills/mongodb-ar21.svg" alt="gallery img 13" className={styles.skillsGalleryItemImg} />
+        <img src="/icons/skills/recoil-js.svg" alt="gallery img 13" className={styles.skillsGalleryItemImg} />
       </figure>
 
       <figure className={styles.skillsGalleryItem}>
-        <img src="/icons/skills/mysql.svg" alt="gallery img 14" className={styles.skillsGalleryItemImg} />
+        <img src="/icons/skills/sass.svg" alt="gallery img 14" className={styles.skillsGalleryItemImg} />
+      </figure>
+
+      <figure className={styles.skillsGalleryItem}>
+        <img src="/icons/skills/jest.svg" alt="gallery img 15" className={styles.skillsGalleryItemImg} />
+      </figure>
+
+      <figure className={styles.skillsGalleryItem}>
+        <img src="/icons/skills/google.svg" alt="gallery img 16" className={styles.skillsGalleryItemImg} />
+      </figure>
+
+      <figure className={styles.skillsGalleryItem}>
+        <img src="/icons/skills/prisma.svg" alt="gallery img 17" className={styles.skillsGalleryItemImg} />
+      </figure>
+
+      <figure className={styles.skillsGalleryItem}>
+        <img src="/icons/skills/rest-api.svg" alt="gallery img 18" className={styles.skillsGalleryItemImg} />
+      </figure>
+
+      <figure className={styles.skillsGalleryItem}>
+        <img src="/icons/skills/wordpress.svg" alt="gallery img 19" className={styles.skillsGalleryItemImg} />
+      </figure>
+
+      <figure className={styles.skillsGalleryItem}>
+        <img src="/icons/skills/stack-overflow.svg" alt="gallery img 21" className={styles.skillsGalleryItemImg} />
+      </figure>
+
+      <figure className={styles.skillsGalleryItem}>
+        <img src="/icons/skills/mongodb.svg" alt="gallery img 20" className={styles.skillsGalleryItemImg} />
+      </figure>
+
+
+      <figure className={styles.skillsGalleryItem}>
+        <img src="/icons/skills/postgresql.svg" alt="gallery img 22" className={styles.skillsGalleryItemImg} />
       </figure>
     </section>
   )
